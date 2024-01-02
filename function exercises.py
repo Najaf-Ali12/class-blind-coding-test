154368341555ea406f5550b587f6253b7a6e4621
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-01-02 17:11:05 +0500

    Add files via upload

---
+++ function exercises.py
@@ -0,0 +1,18 @@
+#using functions make a programme that tells that either
+#inputted number is even or odd
+def checker(x):
+    if x%2==0:
+        return"even"
+    if x%2==1:
+        return"odd"
+number=int(input("enter a number"))
+result=checker(number)
+print("your number is",result)
+def area(w,h):#w=width and H=height
+    return w*h
+width=int(input("please enter the width of rectangle in centimetres"))
+height=int(input("please enter the height of rectangle in centimetres"))
+result=area(width,height)
+print("the area of the rectangle is",result,"centimetres")
+
+
