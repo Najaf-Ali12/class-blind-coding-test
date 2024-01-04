f94e3ff0ff890ba44e6372d058965bbab020ae4d
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-01-04 12:29:01 +0500

    Add files via upload

---
+++ lab5 factorial with both loops.py
@@ -0,0 +1,25 @@
+
+#make a programme that tell the factorial of user inputted number
+#with both for and while loops
+#1:using for loop
+n=int(input("enter a number whose factorial you want"))
+s=1
+if n>0:
+    for i in range(1,n+1):
+      s=s*i
+else:
+    print("for getting factorial number must be postive and not equals to zero")
+print("the factorial of",n,"is",s)
+
+
+
+a=int(input("number=?,whose factorial you want"))
+factorial=1
+if a<0:
+    print("input a positive number")
+else:
+    while a>0:
+        factorial*=a
+        a-=1
+print("the factorial of your given number is",factorial)
+
